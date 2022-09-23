0e466995b9ee8ee765f2d0a72b048733242484b1
1Md-Rakibul-Islam <rakibul199tech@gmail.com>
2022-09-23 23:13:31 +0600

    modal side problem and misteck count dectet problem solved

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ const typeController = (e) => {
 
   // if it is not a valid character like Control/Alt then skip displaying anything
   if (!validLetters.includes(newLetter)) {
-    return;
+    return errorCount = errorCount + 1;
   }
 
   userText += newLetter;
@@ -67,7 +67,7 @@ const gameOver = () => {
   // the current time is the finish time
   // so total time taken is current time - start time
   const finishTime = new Date().getTime();
-  const timeTaken = (finishTime - startTime) / 1000;
+  const timeTaken = Math.floor((finishTime - startTime) / 1000);
 
   // show result modal
   resultModal.innerHTML = "";
